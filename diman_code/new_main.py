9737365b1bf33cbc8f24da247281911a7fed8dca
Dima-Aerokvant <dima.cat.sv@gmail.com>
2024-12-07 16:06:07 +0300

    вариант для дальнейшей доработки в кванте

--- diman_code/new_main.py
+++ diman_code/new_main.py
@@ -59,11 +59,12 @@ def upload_frame():
     b64_string = "data:image/jpg;base64," + b64_string
     return jsonify({'data':b64_string}) # отправляем картинку с выделенной зоной интереса
     
-    
-    
 
 def result_moduls(data_now):
-    video_probab =[],audio_probab = [],text_probab = [],series_pulse = []
+    video_probab =[]
+    audio_probab = []
+    text_probab = []
+    series_pulse = []
 
     for emotion in data_now.video_probabilities:
         video_probab.append([str(emotion), int(data_now.video_probabilities[emotion])]) # переделываем словарь в массив нужна для вывода в диограммы
@@ -92,26 +93,12 @@ def upload_audio():
     audio_segment = AudioSegment.from_file(audio_io, format='webm')  # Декодируем файл
     audio_segment.export("mic.wav", format="wav")  # Сохраняем файл как WAV
 
-    def res_th1(x):
-        global data_now
-        data_now.audio_probabilities = x
-    #это костыль? ДА это костыль да еще какой! получть return из функции находящийся в потоке нельзя и приходится выкручиваться
-    def res_th2(x):
-        global data_now
-        data_now.text_probabilities = x
+    data_now.audio_probabilities = moduls.audio_recognition_text('mic.wav')  # Обработка аудио
+    data_now.text_probabilities = moduls.text_recognition('mic.wav')  # Обработка текст
 
-    thread1 = Thread(target= lambda x : res_th1(moduls.audio_recognition_text(x)), args =('mic.wav'))
-    thread2 = Thread(target= lambda x : res_th2(moduls.text_recognition(x)), args =('mic.wav')) # парралельно обрабатываем аудио с помощью 2ух модулей
-
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
 
     send = result_moduls(data_now)
     return send
-
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
